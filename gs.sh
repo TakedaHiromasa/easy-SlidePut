b22337e6fe74a8b551b5757f129a698a5a89d8aa
TakedaHiromasa <taisyou119@gmail.com>
2019-11-15 04:18:43 +0000

    add commit

---
+++ gs.sh
@@ -0,0 +1,10 @@
+gs \
+   -dMaxBitmap=640000000 \
+   -dMaxPatternBitmap=80000000 \
+   -dNOGC \
+   -sDEVICE=jpeg   \
+   -o TVRD/TVRD%03d.jpg \
+   -dJPEGQ=80      \
+   -r100x100       \
+   -g1424x1068     \
+   TVRD3.pdf
